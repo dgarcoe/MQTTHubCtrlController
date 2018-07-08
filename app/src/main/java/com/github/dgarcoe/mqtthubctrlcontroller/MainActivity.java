9a64c1b0fe6bc763c5a15193b2178b7676b0b6b7
dgarcoe <dannn86m@gmail.com>
2018-07-08 17:59:01 +0200

    Modified layouts

--- app/src/main/java/com/github/dgarcoe/mqtthubctrlcontroller/MainActivity.java
+++ app/src/main/java/com/github/dgarcoe/mqtthubctrlcontroller/MainActivity.java
@@ -3,7 +3,11 @@ package com.github.dgarcoe.mqtthubctrlcontroller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+
 public class MainActivity extends AppCompatActivity {
+
+    MqttAndroidClient mqttAndroidClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
